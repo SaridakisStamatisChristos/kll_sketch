59e234430528715bc684ea0699c98d815e3b5665
SaridakisStamatisChristos <34583142+SaridakisStamatisChristos@users.noreply.github.com>
2026-09-05 18:57:47 +0300

    Wire CPython slot-offset merge experiment

--- kll_sketch/_native_state.cpp
+++ kll_sketch/_native_state.cpp
@@ -1,5 +1,6 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
+#include <structmember.h>
 #undef PyInit__native
 
 #include <algorithm>
@@ -30,6 +31,7 @@ namespace kll_state_addon {
 #include "_native_state_bindings.inc"
 #include "_native_type_fastpaths.inc"
 #include "_native_v32_merge.inc"
+#include "_native_v32_slots.inc"
 } // namespace kll_state_addon
 
 extern "C" PyObject* PyInit__native_base(void);
@@ -39,7 +41,8 @@ PyMODINIT_FUNC PyInit__native(void) {
     if (!module) return nullptr;
     if (PyModule_AddFunctions(module, kll_state_addon::addon_methods) < 0 ||
         PyModule_AddFunctions(module, kll_state_addon::type_fastpath_methods) < 0 ||
-        PyModule_AddFunctions(module, kll_state_addon::v32_merge_methods) < 0) {
+        PyModule_AddFunctions(module, kll_state_addon::v32_merge_methods) < 0 ||
+        PyModule_AddFunctions(module, kll_state_addon::v32_slot_methods) < 0) {
         Py_DECREF(module);
         return nullptr;
     }
